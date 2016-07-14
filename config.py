5e1e39a284125dcf0a0013764ccb412cf804b43a
Jed Simson <jed.simson@gmail.com>
2016-07-14 14:55:12 +1200

    Add ReCaptcha to contact form

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ class Config(object):
     DEBUG = False
     DEVELOPMENT = False
 
-    # Set a secure secret key as an environment
-    # variable
+    # Set a secure secret key as an environment variable
     SECRET_KEY = os.environ['SECRET_KEY']
 
+    # ReCaptcha settings
+    RECAPTCHA_PUBLIC_KEY = os.environ['RECAPTCHA_PUBLIC_KEY']
+    RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY']
 
 class ProductionConfig(Config):
     pass
--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import Form, RecaptchaField
 from wtforms import StringField, TextAreaField
 from wtforms.validators import Required, Email
 from wtforms.fields.html5 import EmailField
@@ -8,3 +8,4 @@ class ContactForm(Form):
     name = StringField('Name', validators=[Required()])
     email = EmailField('Email Address', validators=[Required(), Email()])
     message = TextAreaField('Message', validators=[Required()])
+    recaptcha = RecaptchaField()
