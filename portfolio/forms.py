41e42a04326b8016957d0c87135948ea62aeab79
Jed Simson <jed.simson@gmail.com>
2016-07-14 13:08:04 +1200

    Add contact form skeleton

---
+++ portfolio/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField
+from wtforms.validators import Required, Email
+from wtforms.fields.html5 import EmailField
+
+
+class ContactForm(Form):
+    name = StringField('Name', validators=[Required()])
+    email = EmailField('Email Address', validators=[Required(), Email()])
+    message = TextAreaField('Message', validators=[Required()])
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template
 
+from .forms import ContactForm
+
 portfolio = Blueprint('portfolio', __name__)
 
 
 @portfolio.route('/')
+@portfolio.route('/home/')
+@portfolio.route('/index/')
+@portfolio.route('/index.html')
 def home():
     return render_template('home.html')
+
+
+@portfolio.route('/contact/')
+def contact():
+    form = ContactForm()
+
+    return render_template('contact.html', form=form)
