5871d154b543a9559df4999ae7df86b4501823e0
Jed Simson <jed.simson@gmail.com>
2016-07-23 12:15:22 +1200

    Add webassets

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flaskext.markdown import Markdown
 from flask_compress import Compress
+from flask_assets import Environment, Bundle
 
 from portfolio.views import portfolio
 from config import Config
@@ -16,6 +17,13 @@ def create_app(config=None):
 
     # Enable Flask-Compress for gzipping static files
     Compress(app)
+
+    # Enable Flask-Assets to create bundles for assets
+    assets = Environment(app)
+
+    css = Bundle('css/bootstrap.min.css', 'css/custom.min.css',
+                 filters='cssmin', output='css/app.css')
+    assets.register('css_all', css)
 
     if config:
         # There is a specified configuration
