39a2ceb456c1fa5f6dc3515746ed45a96fd1a205
Jed Simson <jed.simson@gmail.com>
2021-03-14 12:35:28 +1300

    Refactor app configuration, add Flask-Talisman for security.

--- app.py
+++ app.py
@@ -1,44 +1,76 @@
 import os
+from typing import Any, List, Callable
 
 from flask import Flask
 from flaskext.markdown import Markdown
 from flask_compress import Compress
 from flask_assets import Environment, Bundle
-
-from markdown.extensions.meta import MetaExtension
+from flask_talisman import Talisman
 
 import logging
 
-from portfolio.views import portfolio
+from portfolio.views import portfolio as portfolio_blueprint
 from blog import blog_manager
 from util import format_date, format_value
 from config import Config
-from cache import cache
 
 ONE_DAY = 60 * 60 * 24
 
+# Represents the pipeline for configuring the portfolio app.
+PortfolioConfigurationStep = Callable[[Flask], Flask]
+PortfolioConfigurationPipeline = List[PortfolioConfigurationStep]
+
+class PortfolioBuilder:
+    ''' Responsible for building the portfolio app. '''
+
+    def __init__(self, app: Flask, configuration_pipeline: PortfolioConfigurationPipeline):
+        self.app = app
+        self.configuration_pipeline = configuration_pipeline
+
+    def configure(self, configuration: Any) -> Flask:
+        ''' Configures the app and runs the steps specified in the configuration pipeline. '''
+
+        self.app.config.from_object(configuration)
+
+        self.configure_logging()
+
+        self.app.logger.info('Configuring app')
+
+        for step in self.configuration_pipeline:
+            step(self.app)
+
+        self.app.logger.info('App configured')
+
+        return self.app
+
+    def configure_logging(self):
+        logging.basicConfig(level=self.app.config['LOG_LEVEL'])
+
+        self.app.logger.info('App logging configured')
+
 def create_app(config=None):
     app = Flask(__name__)
+    portfolio = PortfolioBuilder(app, [
+        configure_markdown_and_blog,
+        configure_compression_and_asset_bundling,
+        configure_security,
+        configure_blueprints
+    ])
+
+    if config is None:
+        config = os.environ.get('APP_SETTINGS', Config)
+
+    app = portfolio.configure(config)
+
+    return app
+
+def configure_markdown_and_blog(app: Flask) -> Flask:
+    app.logger.debug('Configuring markdown support...')
 
     # Enable Markdown for better/simpler blog posts
     md = Markdown(app, extensions=['markdown.extensions.meta'])
 
-    # Enable Flask-Compress for gzipping static files
-    Compress(app)
-
-    # Enable Flask-Assets to create bundles for assets
-    assets = Environment(app)
-
-    css = Bundle(
-        'css/custom.css',
-        'css/uikit.min.css',
-        filters='cssmin',
-        output='css/app.css'
-    )
-
-    assets.register('css_all', css)
-
-    cache.init_app(app)
+    app.logger.debug('Configuring blog manager')
 
     # Use my custom blogging extension
     blog_manager.init_app(
@@ -48,22 +80,49 @@ def create_app(config=None):
         ONE_DAY, # Invalidate cached posts after 1 day
     )
 
-    if config:
-        # There is a specified configuration
-        app.config.from_object(config)
-    else:
-        app.config.from_object(os.environ.get('APP_SETTINGS', Config))
-
-    # Custom filters
+    # Custom Jinja filters for the blog
     app.jinja_env.filters['format_date'] = format_date
     app.jinja_env.filters['format_value'] = format_value
     app.jinja_env.globals['markdown_instance'] = md._instance
 
-    app.register_blueprint(portfolio)
+    return app
 
-    logging.basicConfig(level=app.config['LOG_LEVEL'])
+def configure_compression_and_asset_bundling(app: Flask) -> Flask:
+    app.logger.debug('Configuring compression for static files...')
 
-    app.logger.info('App configured')
+    # Enable Flask-Compress for gzipping static files
+    Compress(app)
+
+    app.logger.debug('Configuring asset bundling...')
+
+    # Enable Flask-Assets to create bundles for assets
+    assets = Environment(app)
+
+    css = Bundle(
+        'css/custom.css',
+        'css/uikit.min.css',
+        'css/tomorrow.min.css',
+        filters='cssmin',
+        output='css/app.css'
+    )
+
+    assets.register('css_all', css)
+
+    return app
+
+def configure_security(app: Flask) -> Flask:
+    app.logger.debug('Configuring security features')
+
+    # Enable Flask-Talisman to automatically set HTTP headers for web app security issues
+    Talisman(
+        app, 
+        content_security_policy=app.config['CONTENT_SECURITY_POLICY'],
+        content_security_policy_nonce_in=['script-src'])
+
+    return app
+
+def configure_blueprints(app: Flask) -> Flask:
+    app.register_blueprint(portfolio_blueprint)
 
     return app
 
--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+DEFAULT_CONTENT_SECURITY_POLICY = {
+    'default-src': '\'self\' *.spotify.com *.google.com disqus.com *.disqus.com *.disquscdn.com',
+    'style-src': '\'self\' fonts.googleapis.com',
+    'font-src': '\'self\' fonts.gstatic.com',
+    'script-src': '\'self\' *.google-analytics.com *.google.com/recaptcha/api.js *.disqus.com *.disquscdn.com'
+}
 
 class Config(object):
     DEBUG = False
@@ -15,6 +22,9 @@ class Config(object):
 
     # Logging
     LOG_LEVEL = os.environ['LOG_LEVEL']
+
+    # Security
+    CONTENT_SECURITY_POLICY = os.environ.get('CONTENT_SECURITY_POLICY', DEFAULT_CONTENT_SECURITY_POLICY)
 
 
 class ProductionConfig(Config):
