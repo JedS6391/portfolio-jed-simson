07aed86fb15968b969155184a000ab4eeeecf8fb
Jed Simson <jed.simson@gmail.com>
2021-12-23 16:33:16 +1300

    Fix breaking changes in forms library.

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,12 +1,11 @@
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, TextAreaField
-from wtforms.validators import Required, Email
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField, StringField, TextAreaField
+from wtforms.validators import InputRequired, Email
 
 class ContactForm(FlaskForm):
     ''' Form used on the /contact/ route for sending a message to the appropriate contact email. '''
 
-    name = StringField('Name', validators=[Required()])
-    email = EmailField('Email Address', validators=[Required(), Email()])
-    message = TextAreaField('Message', validators=[Required()])
+    name = StringField('Name', validators=[InputRequired()])
+    email = EmailField('Email Address', validators=[InputRequired(), Email()])
+    message = TextAreaField('Message', validators=[InputRequired()])
     recaptcha = RecaptchaField()
